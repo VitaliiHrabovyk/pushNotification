4fdfdeab30207404294eb3e4a8269da561d25066
VitaliiHrabovyk <107185623+VitaliiHrabovyk@users.noreply.github.com>
2022-07-25 21:45:02 +0300

    bodyParser

--- send.js
+++ send.js
@@ -1,22 +1,23 @@
 const express = require('express');
 const webpush = require('web-push');
 var app = express();
-var cors = require('cors')
+var cors = require('cors');
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors({ origin: 'http://localhost:4200'})); // change on deploy
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, PATCH, PUT, POST, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Credentials", true);
     next();  
   });
 
-  app.use(cors({
-    origin: 'http://localhost:4200'
-}));
-
-
 app.get("/", function(request, response){
-    response.send("send service alive");
+    response.send("send service work");
 })
 
 
@@ -33,18 +34,19 @@ webpush.setVapidDetails(
 
 
 
-app.post("/api/send",cors(), function (req, res) {
-    res.header("Access-Control-Allow-Origin", "*");
+app.post("/api/send", function (req, res) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, PATCH, PUT, POST, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Credentials", true);
 
-    console.log(req.query)
-    console.log(req.query.title)
-    console.log(req.query.body)
-    console.log(typeof(req.query.subscribe))
+    console.log(req.body)
+    console.log(req.body.title)
+    console.log(req.body.body)
+    console.log(typeof(req.body.subscribe))
 
     
-    let subscribe = JSON.parse(req.query.subscribe)
+    let subscribe = JSON.parse(req.body.subscribe)
 
     const allSubscriptions = [subscribe]
 
@@ -52,8 +54,8 @@ app.post("/api/send",cors(), function (req, res) {
 
     const notificationPayload = {
         "notification": {
-            "title":req.query.title,
-            "body": req.query.body,
+            "title":req.body.title,
+            "body": req.body.body,
             "icon": "./images/badge.png",
             "vibrate": [100, 50, 100],
             "data": {
@@ -69,7 +71,7 @@ app.post("/api/send",cors(), function (req, res) {
 
     Promise.all(allSubscriptions.map(sub => webpush.sendNotification(
         sub, JSON.stringify(notificationPayload) )))
-        .then(() => res.status(200).json({message: 'Newsletter sent successfully.'}))
+        .then(() => res.status(200).json({message: 'Push sent successfully.'}))
         .catch(err => {
             console.error("Error sending notification, reason: ", err);
             res.sendStatus(500);
